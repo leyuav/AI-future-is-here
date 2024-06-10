6ca672dd4273f0eab1de501140247b27fc4ce1df
leyuav <leyu.av@gmail.com>
2024-06-10 20:09:13 +0200

    Added file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Header, Footer, Blog, Features, Possibility, WhatGPT4 } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg"></div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./whatgpt4.css";
+
+const WhatGPT4 = () => {
+  return <div>WhatGPT4</div>;
+};
+
+export default WhatGPT4;
