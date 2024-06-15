cdacba8b8c78c68b6c4fc2598c2ac1cf4407043b
leyuav <leyu.av@gmail.com>
2024-06-15 12:47:55 +0200

    Stylings for left header section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,14 +6,14 @@ import ai from "../../assets/ai.png";
 const Header = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
-      <div className="gtp3__header-content">
+      <div className="gpt3__header-content">
         <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
         <p>
           Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence
           way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
         </p>
 
-        <div className="gpt3__header-content_input">
+        <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email Address" />
           <button type="button">Get Started</button>
         </div>
@@ -23,9 +23,9 @@ const Header = () => {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
 
-        <div className="gtp3__header-image">
+        {/* <div className="gpt3__header-image">
           <img src={ai} alt="graphic design picture of artificial intelligence" />
-        </div>
+        </div> */}
       </div>
     </div>
   );
