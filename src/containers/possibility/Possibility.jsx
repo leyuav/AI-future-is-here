c30a29bc9229e308954f2dac270b8c7dbc7d63b9
leyuav <leyu.av@gmail.com>
2024-06-15 17:41:56 +0200

    Started possibilities section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import possibilityImage from "../../assets/possibility.png";
 import "./possibility.css";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="picture of a woman wearing VR headset" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus odio laborum deleniti voluptatem quia
+          earum accusamus. Quisquam assumenda totam nisi cumque iusto fugit soluta aut, necessitatibus tenetur ad.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
